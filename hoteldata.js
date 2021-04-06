56e09d5fe3e89189fe4f7a3517c7a40dadfbcc8a
Kanchan Chauhan <kanchan827@gmail.com>
2021-04-05 20:54:54 -0700

    Create hoteldata.js

---
+++ hoteldata.js
@@ -0,0 +1,115 @@
+[
+    {
+        "type": "hotel",
+        "name": "TEST CONTENT",
+        "description": null,
+        "image": [],
+        "checkInDate": "2021-04-06",
+        "checkOutDate": "2021-04-07",
+        "roomDescription": "Flexible Rate, see Rate Rules\nSup' Rm Top floor, 40 sqm king bed, marble bath\nroom, elegant seating area, Floor to ceiling wi",
+        "guests": 1,
+        "price": "774.00"
+    },
+    {
+        "type": "hotel",
+        "name": "CONRAD LONDON ST JAMES",
+        "description": null,
+        "image": [],
+        "checkInDate": "2021-04-06",
+        "checkOutDate": "2021-04-07",
+        "roomDescription": "FLEXIBLE RATE\nKING ACCESSIBLE SUPERIOR ROOM\nWHEELCHAIR ACCESSIBLE ROOM W/ ROLL-IN SHOWER",
+        "guests": 1,
+        "price": "285.00"
+    },
+    {
+        "type": "hotel",
+        "name": "Sheraton Grand London Park Lane",
+        "description": "A celebrated Art Deco property the Sheraton Grand London Park Lane hotel offers a prime destination in the heart of Mayfair. With an ideal location our park lane hotel faces one of the capital's Royal Parks, Green Park which is just a short walk away from Buckingham Palace and Westminster. With comfortable facilities that will make you feel right at home, stay in one of our renovated rooms or suites featuring 32\" HD plasma televisions, free WiFi and our signature Sheraton Signature Sleep experience leaving you energized in the morning. Sit back and relax whilst enjoying one of the best traditional afternoon teas in London and listen to the relaxing sounds of the harp. Visit our Italian Restaurant Mercante to indulge in seasonal creations by head chef Davide D'Ignazio who brings a fresh take on Italian dining in Mayfair. In the evening, sip on expertly crafted cocktails in Mayfair's local, Smith & Whistle.",
+        "image": [
+            "http://uat.multimediarepository.testing.amadeus.com/cmr/retrieve/hotel/69F6E66433B34C4A9A5251EC7ECA11B6"
+        ],
+        "checkInDate": "2021-04-06",
+        "checkOutDate": "2021-04-07",
+        "roomDescription": "Buy 2 rooms and get 50% off 2nd room, Family Offer, see Rate Ru\nDeluxe Room, 1 King, 24sqm/258sqft-25sqm/269sqf\nt, Wireless internet, complimentary, Coffee/tea",
+        "guests": 1,
+        "price": "263.00"
+    },
+    {
+        "type": "hotel",
+        "name": "INTERCONTINENTAL LONDON PARK",
+        "description": "Once the site of a royal residence, InterContinental London Park Lane is now the epitome of modern elegance. Located between Mayfair and Knightsbridge in view of Buckingham Palace, the hotel is not far from the Tower of London Big Ben and Trafalgar Square. Also worth seeing is the Victoria and Albert Museum, Madame Tussauds and The National Portrait Gallery.",
+        "image": [
+            "http://uat.multimediarepository.testing.amadeus.com/cmr/retrieve/hotel/07FEB42E17BC41A0989EE080FD70A73A"
+        ],
+        "checkInDate": "2021-04-06",
+        "checkOutDate": "2021-04-07",
+        "roomDescription": "REWARD NIGHTS\nCLASSIC ROOM FEATURING A TRANQUIL INNER\nCOURTYARD LOCATION CLASSIC RMS FEATURE ELEGANT",
+        "guests": 1,
+        "price": "61.75"
+    },
+    {
+        "type": "hotel",
+        "name": "Belle Cour",
+        "description": "Relax and unwind in the comfortable atmosphere of the Belle Cour Hotel. After an exciting day in the city, you can treat yourself to a visit to the Nail Spa. Then, you can retreat to one of the elegant rooms or apartments and benefit from the free Wi-Fi.",
+        "image": [
+            "http://uat.multimediarepository.testing.amadeus.com/cmr/retrieve/hotel/74C3BC1A4C3A4EA6BA3DC2DC27A3E3D3"
+        ],
+        "checkInDate": "2021-04-06",
+        "checkOutDate": "2021-04-07",
+        "roomDescription": "HRS-Rate - LastMinute-Discount\nStandard room A standard room consists of a room with shower-toilet or bathtub-toilet.",
+        "guests": 1,
+        "price": "139.50"
+    },
+    {
+        "type": "hotel",
+        "name": "HOLIDAY INN EXP SOUTHWARK ST",
+        "description": "Make the Holiday Inn Express London Southwark hotel your base for a buzzing central London location and excellent transport links. Friendly staff welcomes you on your arrival at our intimate 136 room hotel near Londons cosmopolitan South Bank. Holiday Inn Express London Southwark is a short walk from Southwark Underground Station and Blackfriars Network Rail Station with direct train services to Gatwick and Luton Airports. Settle into your air conditioned Guest room  decorated in contemporary colours  and start each day with a complimentary breakfast in our lively Great Room. Getting around London is easy with London Bridge and Waterloo stations just a short stroll from the hotel. Cross Blackfriars Bridge to get to the London Stock Exchange and Bank of England. We're just minutes from the lively cafes  theatres and galleries that dot Londons trendy South Bank. Walk along the River Thames to the Tate Modern gallery housed in a converted power station  or stroll over the futuristic Millennium Bridge to elegant St Pauls Cathedral. Why not stop by Borough Market for food tasting en route to the Shard. Holiday Inn Express London Southwarks central location puts chic bars and restaurants on your doorstep. Ask staff to recommend their favourites or do your own research via free wireless Internet in our open plan Lobby",
+        "image": [
+            "http://uat.multimediarepository.testing.amadeus.com/cmr/retrieve/hotel/BEB226A7324349CA8184D12DB8425311"
+        ],
+        "checkInDate": "2021-04-06",
+        "checkOutDate": "2021-04-07",
+        "roomDescription": "BOOK NOW, PAY LATER\n2 TWIN BEDS ROOM NONSMOKING TAKE YOUR PICK FROM\nAN EXTENSIVE SELECTION OF FREE VIEW CHANNELS ON",
+        "guests": 1,
+        "price": "174.30"
+    },
+    {
+        "type": "hotel",
+        "name": "Gower House",
+        "description": "Situated in a terrace of Georgian town houses, the Gower House Hotel is a family-run retreat in the bustling centre of London.  Its rooms are comfortably furnished and feature modern amenities such as free Wi-Fi. In the morning, a typical English breakfast is served to ensure guests a good start into the day. HRS guests benefit from the late check-out option at the Gower House.",
+        "image": [
+            "http://uat.multimediarepository.testing.amadeus.com/cmr/retrieve/hotel/EA92842E81014CE894898BCDA37002E3"
+        ],
+        "checkInDate": "2021-04-06",
+        "checkOutDate": "2021-04-07",
+        "roomDescription": "Hot Deal\nStandard room A standard room consists of a room with shower-toilet or bathtub-toilet.",
+        "guests": 1,
+        "price": "142.50"
+    },
+    {
+        "type": "hotel",
+        "name": "Staybridge Stes Vauxhall",
+        "description": "A warm and friendly welcome awaits you at Staybridge Suites London Vauxhall hotel located in a quite leafy side street just off Albert Embankment and only minutes away from Vauxhall tube bus and rail links From the moment you step into our bright and contemporary lobby you will feel completely at home This is a hotel where you come to live rather than stay so settle in and unwind by the fireplace in our lounge Head up to your striking modern yet cozy studio or one bedroom suite where you will find all the comforts of home with all the mod cons including wireless internet throughout Staybridge Suites London Vauxhall hotel is superbly located offering stunning views of the London Eye and Westminster as well as being just a short from Londons eclectic Southbank the culture heart of the city and home to Londons most iconic attractions theatres arts and design led venues. The area is buzzing with things to see and do all year round free events music film exhibitions dance theatre debates festivals and all sorts of fun the ideal neighbourhood to stay in when in London  Our complimentary make yourself at home breakfast is served in our Hub Kitchen daily and in the evening our guests can network or mingle over complimentary drinks and snacks at our Managers Social held every Tuesday Wednesday and Thursday After a long day you can energize in the Fitness Centre or meet in the Den meeting room.",
+        "image": [
+            "http://uat.multimediarepository.testing.amadeus.com/cmr/retrieve/hotel/FE1C69C6B474496D83307A37055CFA1D"
+        ],
+        "checkInDate": "2021-04-06",
+        "checkOutDate": "2021-04-07",
+        "roomDescription": "GOVERNMENT RATE\nSTUDIO SUITE 1 KING BED NONSMOKING INCLUDES\nCOMP WIFI AND BREAKFAST. LARGER THAN A STANDARD",
+        "guests": 1,
+        "price": "251.75"
+    },
+    {
+        "type": "hotel",
+        "name": "Holiday Inn Exp Victoria Stn",
+        "description": "Enjoy friendly service with a personal touch in the heart of London at the Holiday Inn Express London Victoria hotel, a short walk from Pimlico train station. Housed in a beautifully restored Victorian building on bustling Belgrave Road, Holiday Inn Express London Victoria is within easy strolling distance of major sights like Buckingham Palace.",
+        "image": [
+            "http://uat.multimediarepository.testing.amadeus.com/cmr/retrieve/hotel/C87DD62348C64342998DADF5E6E032E7"
+        ],
+        "checkInDate": "2021-04-06",
+        "checkOutDate": "2021-04-07",
+        "roomDescription": "BEST FLEXIBLE RATE\n1 DOUBLE BED NONSMOKING CATCH UP ON EMAILS OR\nBROWSE THE NET WITH FREE WIRELESS INTERNET MAKE",
+        "guests": 1,
+        "price": "165.00"
+    }
+]
