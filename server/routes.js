1afea6714f5a55941a772131893187df5a59048d
Kanchan Chauhan <kanchan827@gmail.com>
2021-04-07 19:25:42 -0700

    Update routes.js

--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const controllers = require('/Users/kanchanchauhan/Documents/sei/blueocean/HotelsAPI/server/controllers/index.js');
+const controllers = require("./controllers/index.js");
 
 
 router.get('/v2/shopping/hotel-offers', controllers.hotelOffers.getHotelOffers)
